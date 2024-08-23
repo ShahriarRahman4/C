405c56043ca6d7bc6a1dfd1efd1cf8de4e2e57d3
ShahriarRahman4 <srrifat578@gmail.com>
2024-08-23 21:08:18 +0600

    Sixth Chapter

---
+++ pointer-part4.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void swap(int a,int b);
+int main()
+{
+int x = 3 , y = 5;
+swap(x, y);
+printf("X= %d & Y= %d\n",x,y);
+
+    return 0;
+}
+
+
+void swap(int a,int b)
+{
+    int t = a;
+    a = b;
+    b = t;
+    printf("a = %d & b = %d\n",a,b);
+}
---
+++ pointer-part5.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void swap(int a,int b);
+int main()
+{
+int x = 3 , y = 5;
+_swap(&x,&y);
+printf("X= %d & Y= %d\n",x,y);
+
+    return 0;
+}
+
+
+void _swap(int *a,int *b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+    printf("a = %d & b = %d\n",*a,*b);
+}
